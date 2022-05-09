7c388214d1b8b8d15acc16c7af0ee018d652cb15
Roman Rihter <teledurak@gmail.com>
2022-05-09 19:06:54 +0300

    m1882: set right defconfig name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,7 +8,7 @@ DEVICE_PATH := device/meizu/m1882
 TARGET_OTA_ASSERT_DEVICE := m1882,16th
 
 # Kernel
-TARGET_KERNEL_CONFIG := ../m1882_defconfig
+TARGET_KERNEL_CONFIG := m1882_defconfig
 
 # Proprietary BoardConfig
 -include vendor/meizu/m1882/BoardConfigVendor.mk
